8918839fdd703752b66a33c524067db3876a01b4
Tim_1s3 <timisenmann07@gmail.com>
2026-07-07 20:08:54 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// HINWEIS: Dies ist eine Template-Datei. 
+// Die echten Firebase-Konfigurationsdaten wurden aus Sicherheitsgründen entfernt.
+
+const firebaseConfig = {
+  apiKey: "DEIN_API_KEY_HIER",
+  authDomain: "DEIN_PROJEKT.firebaseapp.com",
+  projectId: "DEIN_PROJEKT_ID",
+  storageBucket: "DEIN_PROJEKT.appspot.com",
+  messagingSenderId: "DEINE_ID",
+  appId: "DEINE_APP_ID"
+};
+
+// firebase.initializeApp(firebaseConfig);
